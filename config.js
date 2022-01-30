b78fba416a8ea6aaaa53fc06622a85499358902d
Olefirenko Oleg <espavoru@gmail.com>
2022-01-30 13:20:12 +0200

    favicon fix (wrong path '/img')

--- config.js
+++ config.js
@@ -32,7 +32,6 @@ let config = {
     // 'src/img/demo-*.{png,svg,jpg,jpeg}': 'img/',
     // 'src/fonts/*': 'fonts/',
     'src/favicon/*.{png,ico,svg,xml,webmanifest}': 'img/favicon',
-    'src/favicon/*.ico': '/',
     // 'node_modules/somePackage/images/*.{png,svg,jpg,jpeg}': 'img/',
   },
   'dir': {
